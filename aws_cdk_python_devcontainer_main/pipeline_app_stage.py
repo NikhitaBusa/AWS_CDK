5156110a7c1cc4a34a573915df173709f445a275
Nikhita Busa <nikhita.busa@ohmconnect.com>
2022-05-26 11:17:19 +0000

    Correcting the import statement

--- aws_cdk_python_devcontainer_main/pipeline_app_stage.py
+++ aws_cdk_python_devcontainer_main/pipeline_app_stage.py
@@ -1,9 +1,9 @@
 import aws_cdk as cdk
 from constructs import Construct
-from aws_cdk_python_devcontainer_main.aws_cdk_python_devcontainer_main_stack import MyLambdaStack
+from aws_cdk_python_devcontainer_main.aws_cdk_python_devcontainer_main_stack import AwsCdkPythonDevcontainerMainStack
 
 class MyPipelineAppStage(cdk.Stage):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        lambdaStack = MyLambdaStack(self, "LambdaStack")
+        lambdaStack = AwsCdkPythonDevcontainerMainStack(self, "LambdaStack")
